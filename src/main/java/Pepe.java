07141147a94cc1ff13a27c5236e17e34f7d29ba7
Clement Sim <clementsim333@gmail.com>
2025-08-20 14:19:39 +0800

    Echo

--- src/main/java/Pepe.java
+++ src/main/java/Pepe.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Pepe {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        Scanner scanner = new Scanner(System.in);
+        String border = "____________________________________________________________";
+        String input;
+
+        System.out.println(border);
         System.out.println("Hello, I am Pepe!\n" + "How may I help you today?" );
+        System.out.println(border);
+
+        while (true) {
+            input = scanner.nextLine();
+
+            if(input.equalsIgnoreCase("bye")) {
+                System.out.println(border);
+                System.out.println("Aww...so sad to see you leave! :(");
+                System.out.println(border);
+                break;
+            } else {
+                System.out.println(border);
+                System.out.println(" " + input);
+                System.out.println(border);
+            }
+        }
+
+        scanner.close();
     }
 }
